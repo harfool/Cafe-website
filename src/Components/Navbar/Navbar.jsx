4cb6bafbbb5939b784c2ed12b070a7393256b4a9
harfool gujjar <110320932+harfool@users.noreply.github.com>
2024-09-14 23:24:01 +0530

    first

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    Chill out <div class="card">
+        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Navbar
